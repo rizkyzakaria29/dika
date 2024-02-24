7be92dc3183a185f225816b213fe325d0351af1b
Hakim112211 <maulanahakimrizki@gmail.com>
2024-02-24 17:42:28 +0700

    teruskan

--- pages/kasir/manage_product.php
+++ pages/kasir/manage_product.php
@@ -16,9 +16,32 @@ $result = $conn->query($query);
 <head>
     <meta charset=UTF-8>
     <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>Manage | Cashier</title>
+    <link rel="icon" href="../../assets/images/ocean.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link rel="stylesheet" href="../../assets/style/manage_product.css">
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+  <img src="../../assets/images/ocean.png" alt="ocean" width="30" height="24" class="d-inline-block align-text-top">
+    <a class="navbar-brand" href="../../pages/dashboard.php">Dashboard</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="">Manage</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
     <div class="header">
         <h1>Hello, <?php echo htmlspecialchars($_SESSION['nama']); ?>! Welcome to product management!
 </h1>
--- pages/register.php
+++ pages/register.php
@@ -8,6 +8,7 @@ require_once('../db/DB_register.php');
     <meta charset= "UTF-8">
     <meta name="viewport" content="width-device-width, initial-scale-1.0">
     <title>btr | Register</title>
+    <link rel="icon" href="./assets/images/ocean.png" type="image/x-icon">
     <link rel="stylesheet" href="../assets/style/register.css">
 </head>
 <body>
